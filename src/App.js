8778215b263805f8ceae59ea0e1ee9fee99e7c8d
jmcasebier <jason.beercase@gmail.com>
2019-06-03 17:49:30 -0500

    added app component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>React Native - App 2</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
