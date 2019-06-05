bd1e61fb510b6b41c8d28c46ad16d503112bd881
jmcasebier <jason.beercase@gmail.com>
2019-06-04 22:15:11 -0500

    added login and logout functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
-componentWillMount() {
-  firebase.initializeApp({
-    apiKey: 'AIzaSyBIH2CmyJUvfojYWhmAV6Lzml79JM64isM',
-    authDomain: 'auth-7f1c0.firebaseapp.com',
-    databaseURL: 'https://auth-7f1c0.firebaseio.com',
-    projectId: 'auth-7f1c0',
-    storageBucket: 'auth-7f1c0.appspot.com',
-    messagingSenderId: '562548220283',
-    appId: '1:562548220283:web:1b6457a4229982e6'
-  });
-}
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBIH2CmyJUvfojYWhmAV6Lzml79JM64isM',
+      authDomain: 'auth-7f1c0.firebaseapp.com',
+      databaseURL: 'https://auth-7f1c0.firebaseio.com',
+      projectId: 'auth-7f1c0',
+      storageBucket: 'auth-7f1c0.appspot.com',
+      messagingSenderId: '562548220283',
+      appId: '1:562548220283:web:1b6457a4229982e6'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner />;
+    }
+  }
 
   render() {
     return (
       <View>
         <Header headerText='Authentication' />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
