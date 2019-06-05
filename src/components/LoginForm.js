b6ec10f4cf0089e2e95929073126b761754f4838
jmcasebier <jason.beercase@gmail.com>
2019-06-04 21:28:32 -0500

    added spinner component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import firebase from 'firebase';
-import { Button, Card, CardSection, Input } from './common';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 
 class LoginForm extends Component {
-  state = { email: '', password: '' };
+  state = { email: '', password: '', error: '', loading: false };
 
   onButtonPress() {
     const { email, password } = this.state;
 
-    firebase.auth().signInWithEmailAndPassword(email, password);
+    this.setState({ error: '', loading: true });
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            this.setState({ error: 'Authentication Failed.' });
+          });
+      });
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return <Spinner size='small' />;
+    }
+
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+        Log In
+      </Button>
+    );
   }
 
   render() {
@@ -31,14 +52,23 @@ class LoginForm extends Component {
           onChangeText={password => this.setState({ password })}
         />
       </CardSection>
+      <Text style={styles.errorTextStyle}>
+        {this.state.error}
+      </Text>
       <CardSection>
-        <Button onPress={this.onButtonPress.bind(this)}>
-          Log In
-        </Button>
+        {this.renderButton()}
       </CardSection>
       </Card>
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 export default LoginForm;
