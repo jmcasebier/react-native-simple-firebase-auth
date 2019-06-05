58f3a6a44264e9145baa07ce24584b1ac709b779
jmcasebier <jason.beercase@gmail.com>
2019-06-04 22:28:20 -0500

    minor fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header, Button, Spinner } from './components/common';
+import { Card, CardSection, Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
@@ -31,14 +31,24 @@ class App extends Component {
     switch (this.state.loggedIn) {
       case true:
         return (
-          <Button onPress={() => firebase.auth().signOut()}>
-            Log Out
-          </Button>
+          <Card>
+            <CardSection>
+              <Button onPress={() => firebase.auth().signOut()}>
+                Log Out
+              </Button>
+            </CardSection>
+          </Card>
         );
       case false:
         return <LoginForm />;
       default:
-        return <Spinner />;
+        return (
+          <Card>
+            <CardSection>
+              <Spinner />
+            </CardSection>
+          </Card>
+        );
     }
   }
 
