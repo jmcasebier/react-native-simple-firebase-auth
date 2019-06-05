d945fe1094b92ee6f953af934b98083ea98c81a0
jmcasebier <jason.beercase@gmail.com>
2019-06-04 20:17:54 -0500

    added email and password fields

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,22 +1,38 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { text: '' };
+  state = { email: '', password: '' };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    firebase.auth().signInWithEmailAndPassword(email, password);
+  }
 
   render() {
     return (
       <Card>
       <CardSection>
         <Input
+          placeholder='john_doe@gmail.com'
           label='Email'
-          value={this.state.text}
-          onChangeText={text => this.setState({ text })}
+          value={this.state.email}
+          onChangeText={email => this.setState({ email })}
         />
       </CardSection>
-      <CardSection />
       <CardSection>
-        <Button>
+        <Input
+          secureTextEntry
+          placeholder='password'
+          label='Password'
+          value={this.state.password}
+          onChangeText={password => this.setState({ password })}
+        />
+      </CardSection>
+      <CardSection>
+        <Button onPress={this.onButtonPress.bind(this)}>
           Log In
         </Button>
       </CardSection>
